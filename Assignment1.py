cedc5c4e73c1474dfc4d1fe7143ce2f8024a6117
Kevin Huang <huangk11@uci.edu>
2020-04-26 23:10:49 -0700

    Finished the three functions. Now I need to work on making the three plots

---
+++ Assignment1.py
@@ -0,0 +1,144 @@
+# Assignment1.py
+# Kevin Huang
+# huangk11@uci.edu
+
+#!/usr/bin/env python
+
+import glob
+import numpy as np
+
+
+import os
+import pylab as pp
+
+def ReadPdb( PdbFile ):
+    """Reads a PDB file with a name provided in the argument. 
+       Assumes the PDB file contains only ATOM and TER entries; TER entries are skipped.
+       
+       RETURNS:
+        - Pos, an (Nx3) dimensional NumPy array of positions of alpha carbons
+        - ResNames, a list of residue names.
+    """
+    
+    #ENTER YOUR CODE TO GET Pos, ResNames here. Manipulate them until you get them in the required format, then use the return statement below to return these... 
+    ResNames = []
+
+     
+    with open(PdbFile) as pdb:
+        for line in pdb:
+            line = line.split()
+            
+            # that means line is "TER" or the end
+            if len(line) == 1:
+                break
+            
+            atom_name = line[2]
+
+            if atom_name == "CA":
+                ResName = line[3]
+                ResNames.append(ResName)
+                 
+                x_coord = float(line[5])
+                y_coord = float(line[6])
+                z_coord = float(line[7])
+
+                coords =  np.array([x_coord, y_coord, z_coord])
+                
+                # this allows subsequent coordinates to be stacked below
+                if line[4] == '1':                      
+                    Pos = coords
+                else:
+                    Pos = np.vstack((Pos, coords)) # stack Pos on top of newly created array
+
+    #Return
+    return Pos, ResNames
+
+
+def ResHydrophobic( ResNames ):
+    """Take list of three letter codes for residue names; 
+       returns a list, IsPhobic, of True/False values 
+       depending on whether residues are hydrophobic or not."""
+
+    #Enter your code to identify whether residues or hydrophobic or not here.
+    #Use the return statement below to return them.
+    
+    def isHydrophobic(residue):
+        return residue in {"ALA", "CYS", "PHE", "ILE", "LEU", "MET", "PRO", "VAL", "TRP"}
+
+    return [isHydrophobic(i) for i in ResNames]
+
+
+def RadiusOfGyration( Pos ):
+    """Take an Nx3 dimensional array of positions; return the radius of gyration."""
+
+    #Enter your code to compute the radius of gyration here. 
+    #Use the return statement below to return it. 
+    N = Pos.shape[0] # get the number of rows in Pos
+    
+    # find r bar
+    r_bar = np.zeros(3)
+    
+    for i in Pos:
+        r_bar += i
+
+    r_bar = np.divide(r_bar, N)
+     
+    # find r-sub g
+    r_g_squared = 0
+
+    for r_i in Pos:
+        r_g_squared += np.power(np.linalg.norm(r_i - r_bar), 2)
+    
+    r_g_squared = np.divide(r_g_squared, N)
+    Rg = np.sqrt(r_g_squared)
+    return Rg
+
+
+#SOME SAMPLE SKELETON CODE TO SET UP USE OF THOSE FUNCTIONS FOLLOWS BELOW
+
+#This next if statement is optional; this specifies a block of code that will only execute if the Python program is RUN but NOT if it is imported. Since our intent here is to run the code (not import it) this is unnecessary, but sometimes this is useful.
+if __name__=='__main__':
+   
+    #Obtain list of pdbfiles
+    pdbfiles = glob.glob('*.pdb')
+
+    #Create some empty lists for storing data
+    Rglist = []
+    Rgphobiclist = []
+    nres = []
+
+    #YOUR CODE GOES HERE -- YOU WOULD SET UP A LOOP OVER THE PDB FILES AND DO THE CALCULATIONS
+    #Note for testing purposes you should pick a particular PDB file and try your functions on it. 
+    #In fact, the function for checking residue names can be tested without a PDB file...
+    for pdb in pdbfiles:
+        Pos, ResNames = ReadPdb(pdb)
+        hydrophobicIndices = []
+        
+        isPhobic = ResHydrophobic(ResNames)
+         
+        # we need to figure out which indices contain the hydrophobic residues
+        for i in range(len(isPhobic)):
+            if isPhobic[i]:
+                hydrophobicIndices.append(i)
+        
+        # radius of gyration of all residues
+        Rg = RadiusOfGyration(Pos)
+                
+        # radius of gyration of only hydrophobic residues
+        PhobicPos = Pos[hydrophobicIndices[0]]
+        
+        for i in hydrophobicIndices[1:]:
+            PhobicPos = np.vstack((PhobicPos, Pos[i]))
+        
+        Rgphobic = RadiusOfGyration(PhobicPos)
+        
+        # total number of residues
+        N = Pos.shape[0]
+        
+        # store them
+        Rglist.append(Rg)
+        Rgphobiclist.append(Rgphobic)
+        nres.append(N)
+
+    #ONCE YOU ARE DONE LOOKING AT ALL PDB FILES, YOU WANT TO MAKE SOME PLOTS.
+    
--- template.py
+++ template.py
@@ -6,11 +6,13 @@ import os
 import pylab as pp
 
 def ReadPdb( PdbFile ):
-    """Reads a PDB file with a name provided in the argument. Assumes the PDB file contains only ATOM and TER entries; TER entries are skipped.
-RETURNS:
-- Pos, an (Nx3) dimensional NumPy array of positions of alpha carbons
-- ResNames, a list of residue names.
-"""
+    """Reads a PDB file with a name provided in the argument. 
+       Assumes the PDB file contains only ATOM and TER entries; TER entries are skipped.
+       
+       RETURNS:
+        - Pos, an (Nx3) dimensional NumPy array of positions of alpha carbons
+        - ResNames, a list of residue names.
+    """
 
     #ENTER YOUR CODE TO GET Pos, ResNames here. Manipulate them until you get them in the required format, then use the return statement below to return these... 
 
@@ -20,7 +22,9 @@ RETURNS:
 
 
 def ResHydrophobic( ResNames ):
-    """Take list of three letter codes  for residue names; returns a list, IsPhobic, of True/False values depending on whether residues are hydrophobic or not."""
+    """Take list of three letter codes for residue names; 
+       returns a list, IsPhobic, of True/False values 
+       depending on whether residues are hydrophobic or not."""
 
     #Enter your code to identify whether residues or hydrophobic or not here. Use the return statement below to return them.
 
@@ -49,7 +53,8 @@ if __name__=='__main__':
     nres = []
 
     #YOUR CODE GOES HERE -- YOU WOULD SET UP A LOOP OVER THE PDB FILES AND DO THE CALCULATIONS
-    #Note for testing purposes you should pick a particular PDB file and try your functions on it. In fact, the function for checking residue names can be tested without a PDB file...
+    #Note for testing purposes you should pick a particular PDB file and try your functions on it. 
+    #In fact, the function for checking residue names can be tested without a PDB file...
 
 
 
